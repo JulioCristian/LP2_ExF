c8183a087a66fbc3d7d0d3b8b9347806bbda2d56
Julio <jcrf43@gmail.com>
2024-07-04 04:12:28 -0500

    perdi el progreso

---
+++ src/main/java/com/example/demo/Lp2_ExFApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2_ExFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2_ExFApplication.class, args);
+	}
+
+}
